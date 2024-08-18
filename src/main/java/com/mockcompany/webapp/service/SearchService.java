117af6d916f79987d432f32ca98e049057e424a7
meyamaria <meyatony27@gamil.com>
2024-08-18 12:34:27 +0530

    success build

--- src/main/java/com/mockcompany/webapp/service/SearchService.java
+++ src/main/java/com/mockcompany/webapp/service/SearchService.java
@@ -17,28 +17,28 @@ public class SearchService
 public Collection<ProductItem> search( Iterable<ProductItem> allItems, String query) 
     {
 
-        return Collections.emptyList();
-        // String lowerCaseQuery = query.toLowerCase();
+        // return Collections.emptyList();
+        String lowerCaseQuery = query.toLowerCase();
 
-        // List<ProductItem> itemList = new ArrayList<>();
+        List<ProductItem> itemList = new ArrayList<>();
 
-        // // This is a loop that the code inside will execute on each of the items from the database.
-        // for (ProductItem item : allItems) 
-        // {
-        //     // TODO: Figure out if the item should be returned based on the query parameter!
-        //     if (item.getName().toLowerCase().contains(lowerCaseQuery) ||
-        //     item.getDescription().toLowerCase().contains(lowerCaseQuery) ||
-        //     item.getImage().toLowerCase().contains(lowerCaseQuery) ||
-        //     String.valueOf(item.getCost()).contains(lowerCaseQuery) ||
-        //     String.valueOf(item.getId()).contains(lowerCaseQuery)) 
-        //     {
-        //         Boolean matchesSearch = true;
-        //         itemList.add(item);
-        //         break;
-        //     }
+        // This is a loop that the code inside will execute on each of the items from the database.
+        for (ProductItem item : allItems) 
+        {
+            // TODO: Figure out if the item should be returned based on the query parameter!
+            if (item.getName().toLowerCase().contains(lowerCaseQuery) ||
+            item.getDescription().toLowerCase().contains(lowerCaseQuery) ||
+            item.getImage().toLowerCase().contains(lowerCaseQuery) ||
+            String.valueOf(item.getCost()).contains(lowerCaseQuery) ||
+            String.valueOf(item.getId()).contains(lowerCaseQuery)) 
+            {
+                Boolean matchesSearch = true;
+                itemList.add(item);
+                break;
+            }
           
-        // }
-        // return itemList;
+        }
+        return itemList;
     }
 }
 
